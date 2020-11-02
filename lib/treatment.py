a0b90fa03c38b20d457d0359a8efa511b635cfde
Hua Zheng <zhenghuazx@outlook.com>
2020-11-02 12:43:14 -0500

    add treatment.py

---
+++ lib/treatment.py
@@ -0,0 +1,161 @@
+hypertension_treatments = {'antihypertensives,_ace_inhibitors', 'calcium_channel_blocking_agents',
+                           'beta-adrenergic_blocking_agents', 'angiotensin_receptor_antag.-thiazide_diuretic_comb',
+                           'antihypertensives,_angiotensin_receptor_antagonist', 'thiazide_and_related_diuretics',
+                           'potassium_sparing_diuretics_in_combination', 'alpha/beta-adrenergic_blocking_agents',
+                           'antihyperlipid-_hmg-coa_ri-calcium_channel_blocker',
+                           'ace_inhibitor-thiazide_or_thiazide-like_diuretic',
+                           'angioten.receptr_antag-calcium_chanl_blkr-thiazide',
+                           'angiotensin_receptor_blockr-calcium_channel_blockr',
+                           'miotics_and_other_intraocular_pressure_reducers',
+                           'renin_inhibitor,direct-angiotensin_receptr_antagon',
+                           'angiotensin_recept-neprilysin_inhibitor_comb(arni)',
+                           'beta-blockers_and_thiazide,thiazide-like_diuretics', 'bulk_chemicals',
+                           'renin_inhibitor,direct_and_thiazide_diuretic_comb',
+                           'anti-inflammatory,_interleukin-1_beta_blockers'}
+
+dm_treatment = {'antihyperglycemic,_biguanide_type', 'insulins',
+                'antihyperglycemic,insulin-release_stim.-biguanide',
+                'antihyperglycemic,_dpp-4_inhibitors',
+                'antihypergly,incretin_mimetic(glp-1_recep.agonist)',
+                'antihyperglycemic,_insulin-release_stimulant_type',
+                'antihyperglycemic,dpp-4_inhibitor-biguanide_combs.',
+                'antihyperglycemc-sod/gluc_cotransport2(sglt2)inhib',
+                'antihyperglycemic,_alpha-glucosidase_inhibitors',
+                'antihyperglycemic,_thiazolidinedione_and_biguanide',
+                'antihyperglycemic,thiazolidinedione(pparg_agonist)',
+                'antihyperglycemic_-_dopamine_receptor_agonists',
+                'antihyperglycemic-sglt2_inhibitor-biguanide_combs.',
+                'antihyperglycemic,_sglt-2_and_dpp-4_inhibitor_comb',
+                'antihyperglycemic,_thiazolidinedione-sulfonylurea',
+                'antihypergly,dpp-4_enzyme_inhib.-thiazolidinedione',
+                'antihypergly,insulin,long_act-glp-1_recept.agonist',
+                'antihyperglycemic,_amylin_analog-type'}
+
+ascvd_treatment = {'antihyperlip.hmg_coa_reduct_inhib-cholest.ab.inhib',
+                   'antihyperlipidemic-hmg_coa_reductase_inhib.-niacin',
+                   'antihyperlipidemic_-_hmg_coa_reductase_inhibitors',
+                   'antihyperlipidemic_-_pcsk9_inhibitors',
+                   'bile_salt_sequestrants',
+                   'lipotropics'}
+
+hypertension_treatments_history = ['antihypertensives,_ace_inhibitors_cur',
+ 'calcium_channel_blocking_agents_cur',
+ 'beta-adrenergic_blocking_agents_cur',
+ 'angiotensin_receptor_antag.-thiazide_diuretic_comb_cur',
+ 'antihypertensives,_angiotensin_receptor_antagonist_cur',
+ 'thiazide_and_related_diuretics_cur',
+ 'potassium_sparing_diuretics_in_combination_cur',
+ 'alpha/beta-adrenergic_blocking_agents_cur',
+ 'antihyperlipid-_hmg-coa_ri-calcium_channel_blocker_cur',
+ 'ace_inhibitor-thiazide_or_thiazide-like_diuretic_cur',
+ 'angioten.receptr_antag-calcium_chanl_blkr-thiazide_cur',
+ 'angiotensin_receptor_blockr-calcium_channel_blockr_cur',
+ 'miotics_and_other_intraocular_pressure_reducers_cur',
+ 'renin_inhibitor,direct-angiotensin_receptr_antagon_cur',
+ 'angiotensin_recept-neprilysin_inhibitor_comb(arni)_cur',
+ 'beta-blockers_and_thiazide,thiazide-like_diuretics_cur',
+ 'bulk_chemicals_cur',
+ 'renin_inhibitor,direct_and_thiazide_diuretic_comb_cur',
+ 'anti-inflammatory,_interleukin-1_beta_blockers_cur',
+ 'antihypertensives,_ace_inhibitors_hist',
+ 'calcium_channel_blocking_agents_hist',
+ 'beta-adrenergic_blocking_agents_hist',
+ 'angiotensin_receptor_antag.-thiazide_diuretic_comb_hist',
+ 'antihypertensives,_angiotensin_receptor_antagonist_hist',
+ 'thiazide_and_related_diuretics_hist',
+ 'potassium_sparing_diuretics_in_combination_hist',
+ 'alpha/beta-adrenergic_blocking_agents_hist',
+ 'antihyperlipid-_hmg-coa_ri-calcium_channel_blocker_hist',
+ 'ace_inhibitor-thiazide_or_thiazide-like_diuretic_hist',
+ 'angioten.receptr_antag-calcium_chanl_blkr-thiazide_hist',
+ 'angiotensin_receptor_blockr-calcium_channel_blockr_hist',
+ 'miotics_and_other_intraocular_pressure_reducers_hist',
+ 'renin_inhibitor,direct-angiotensin_receptr_antagon_hist',
+ 'angiotensin_recept-neprilysin_inhibitor_comb(arni)_hist',
+ 'beta-blockers_and_thiazide,thiazide-like_diuretics_hist',
+ 'bulk_chemicals_hist',
+ 'renin_inhibitor,direct_and_thiazide_diuretic_comb_hist',
+ 'anti-inflammatory,_interleukin-1_beta_blockers_hist']
+
+dm_treatment_history = ['antihyperglycemic,_biguanide_type_cur',
+ 'insulins_cur',
+ 'antihyperglycemic,insulin-release_stim.-biguanide_cur',
+ 'antihyperglycemic,_dpp-4_inhibitors_cur',
+ 'antihypergly,incretin_mimetic(glp-1_recep.agonist)_cur',
+ 'antihyperglycemic,_insulin-release_stimulant_type_cur',
+ 'antihyperglycemic,dpp-4_inhibitor-biguanide_combs._cur',
+ 'antihyperglycemc-sod/gluc_cotransport2(sglt2)inhib_cur',
+ 'antihyperglycemic,_alpha-glucosidase_inhibitors_cur',
+ 'antihyperglycemic,_thiazolidinedione_and_biguanide_cur',
+ 'antihyperglycemic,thiazolidinedione(pparg_agonist)_cur',
+ 'antihyperglycemic_-_dopamine_receptor_agonists_cur',
+ 'antihyperglycemic-sglt2_inhibitor-biguanide_combs._cur',
+ 'antihyperglycemic,_sglt-2_and_dpp-4_inhibitor_comb_cur',
+ 'antihyperglycemic,_thiazolidinedione-sulfonylurea_cur',
+ 'antihypergly,dpp-4_enzyme_inhib.-thiazolidinedione_cur',
+ 'antihypergly,insulin,long_act-glp-1_recept.agonist_cur',
+ 'antihyperglycemic,_amylin_analog-type_cur',
+ 'antihyperglycemic,_biguanide_type_hist',
+ 'insulins_hist',
+ 'antihyperglycemic,insulin-release_stim.-biguanide_hist',
+ 'antihyperglycemic,_dpp-4_inhibitors_hist',
+ 'antihypergly,incretin_mimetic(glp-1_recep.agonist)_hist',
+ 'antihyperglycemic,_insulin-release_stimulant_type_hist',
+ 'antihyperglycemic,dpp-4_inhibitor-biguanide_combs._hist',
+ 'antihyperglycemc-sod/gluc_cotransport2(sglt2)inhib_hist',
+ 'antihyperglycemic,_alpha-glucosidase_inhibitors_hist',
+ 'antihyperglycemic,_thiazolidinedione_and_biguanide_hist',
+ 'antihyperglycemic,thiazolidinedione(pparg_agonist)_hist',
+ 'antihyperglycemic_-_dopamine_receptor_agonists_hist',
+ 'antihyperglycemic-sglt2_inhibitor-biguanide_combs._hist',
+ 'antihyperglycemic,_sglt-2_and_dpp-4_inhibitor_comb_hist',
+ 'antihyperglycemic,_thiazolidinedione-sulfonylurea_hist',
+ 'antihypergly,dpp-4_enzyme_inhib.-thiazolidinedione_hist',
+ 'antihypergly,insulin,long_act-glp-1_recept.agonist_hist',
+ 'antihyperglycemic,_amylin_analog-type_hist']
+
+def framingham_ascvd_risk(x):
+    if x[0] == 0:
+        AgeFactor = 2.32888
+        TotalCholFactor = 1.20904
+        HDLCholFactor = -0.70833
+        if x[6]:
+            SysBPFactor = 2.82263
+        else:
+            SysBPFactor = 2.76157
+
+        if x[5] == 1.0:
+            Cig = 0.52873
+        else:
+            Cig = 0.0
+
+        if x[7]:
+            DM = 0.69154
+        else:
+            DM = 0.0
+        AvgRisk = 26.1931
+        RiskPeriodFactor = 0.95012
+    else:
+        AgeFactor = 3.06117
+        TotalCholFactor = 1.12370
+        HDLCholFactor = -0.93263
+        if x[6]:
+            SysBPFactor = 1.99881
+        else:
+            SysBPFactor = 1.93303
+        if x[5] == 1.0:
+            Cig = 0.65451
+        else:
+            Cig = 0.0
+
+        if x[7]:
+            DM = 0.57367
+        else:
+            DM = 0.0
+        AvgRisk = 23.9802
+        RiskPeriodFactor = 0.88936
+
+    risk_factors = (np.log(x[1]) * AgeFactor) + (np.log(x[2]) * TotalCholFactor) + (np.log(x[3]) * HDLCholFactor) + (
+                np.log(x[4]) * SysBPFactor) + Cig + DM - AvgRisk
+    risk = 100 * (1 - np.power(RiskPeriodFactor, np.exp(risk_factors)))
+    return (risk)
